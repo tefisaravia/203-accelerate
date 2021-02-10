9644406f513d76871a6c2e87c57fd7983876cdd4
tefi_saravia <tefisaraviavirtual@gmail.com>
2021-02-10 10:41:36 -0600

    initial commit

---
+++ wp-content/themes/accelerate-theme-child/page-about.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * The template for displaying all pages
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site will use a
+ * different template.
+ * 
+ * <?php Template Name: About Page Template ?>
+ *
+ * @package WordPress
+ * @subpackage Accelerate Marketing
+ * @since Accelerate Marketing 2.0
+ */
+
+get_header(); ?>
+
+
+
+	<section id="services">
+			<?php the_content();?>
+	</section><!--#services-->
+
+
+<?php get_footer(); ?>
